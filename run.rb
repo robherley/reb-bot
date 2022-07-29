1aa493d2d4b72951b7c0bd0040f74da79837a100
Rob Herley <robherley@github.com>
2022-07-28 21:06:42 -0400

    bug fixes, aliases and httpcat

--- run.rb
+++ run.rb
@@ -22,11 +22,11 @@ bot.command :ping, description: 'test if reb-bot is online' do |event|
 end
 
 bot.command :fig, description: 'runs figlet on text' do |_event, *args|
-  "```\n#{Artii::Base.new.asciify args.join(' ')[0..20]}\n```"
+  "```\n#{Artii::Base.new.asciify args.join(' ')[0..20]}```"
 end
 
 bot.command :cow, description: 'cowsay something' do |_event, *args|
-  "```\n#{Cow.new.say args.join(' ')[0..20]}\n```"
+  "```\n#{Cow.new.say args.join(' ')[0..20]}```"
 end
 
 bot.command :ghstat, description: 'reports github status' do |event|
@@ -50,7 +50,7 @@ bot.command :ghstat, description: 'reports github status' do |event|
   end.compact.sort.join("\n")
 end
 
-bot.command :stonk, description: 'get some stonks for tickers' do |event, *args|
+bot.command :stonk, aliases: [:stonks], description: 'get some stonks for tickers' do |event, *args|
   meta = nil
   args.each do |arg|
     if arg == '--meta'
@@ -69,6 +69,26 @@ bot.command :stonk, description: 'get some stonks for tickers' do |event, *args|
   "```#{meta}```" if meta
 end
 
+bot.command :http, description: 'describe http status code' do |event, *args|
+  next unless args.first
+
+  tmp = Tempfile.new('httpcat')
+  begin
+    response = Faraday.get("https://http.cat/#{args.first[0..2]}")
+
+    if response.headers['content-type'] == 'image/jpeg'
+      tmp.write(response.body)
+      tmp.rewind
+      event.send_file(tmp, filename: 'httpcat.jpg')
+    else
+      event.send '🤷 🐈'
+    end
+  ensure
+    tmp.close
+    tmp.unlink
+  end
+end
+
 bot.minecraft :mc, host: 'mc.reb.gg'
 
 bot.react on: %w[reb-bot reb bot], with: %w[🤖 🔥 😈]
@@ -81,5 +101,6 @@ bot.react on: 'game', with: %w[🕹️ ✨ 🃏]
 bot.react on: %w[mc minecraft craft], with: %w[⛏ 💎 ⚔️]
 bot.react on: %w[nert nertz nerts], with: %w[❤️ 🃏]
 bot.react on: 'raft', with: %w[🚣 🪵 🎣 🦈]
+bot.react on: 'ibm', with: %w[👁️ 🐝 Ⓜ️]
 
 bot.run
